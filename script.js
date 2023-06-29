9c38ae9e175164ac11c5adf0682cb61e93df3872
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-29 20:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
-
-  // write your code here
-	function numberOfBooksRead(library) {
+function numberOfBooksRead(library) {
   let count = 0;
-
-  for (let book in library) {
-    if (library.hasOwnProperty(book) && library[book].hasOwnProperty("read") && library[book].read) {
+  for (let i = 0; i < library.length; i++) {
+    if (library[i].readingStatus === true) {
       count++;
     }
   }
-
   return count;
 }
-};
+let library = [
+  {
+    author: "Author 1",
+    title: "Book 1",
+    readingStatus: true
+  },
+  {
+    author: "Author 2",
+    title: "Book 2",
+    readingStatus: false
+  },
+  {
+    author: "Author 3",
+    title: "Book 3",
+    readingStatus: true
+  }
+];
 
-// Do not change the code below
-
-alert(numberOfBooksRead());
+console.log(numberOfBooksRead(library)); // Output: 2
